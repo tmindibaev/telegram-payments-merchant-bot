6300c82b3981cc36533448ef9f8aac07342350f4
Mindibaev Timofei <t.mindibaev@qiwi.com>
2021-11-17 17:25:54 +0300

    added invoice creation and preCheckoutQuery

--- src/main/kotlin/tmin/examples/telegram_payments_merchant_bot/BotStarter.kt
+++ src/main/kotlin/tmin/examples/telegram_payments_merchant_bot/BotStarter.kt
@@ -3,9 +3,14 @@ package tmin.examples.telegram_payments_merchant_bot
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.preCheckoutQuery
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.payments.LabeledPrice
+import com.github.kotlintelegrambot.entities.payments.PaymentInvoiceInfo
 import org.springframework.stereotype.Component
+import java.math.BigInteger
+import java.util.logging.Logger
 
 @Component
 class BotStarter {
@@ -13,7 +18,8 @@ class BotStarter {
     init {
         val bot = bot {
 
-            token = "My token"
+            token = "my bot token"
+
             dispatch {
                 text {
                     if (!availableCommands.contains(text)) {
@@ -24,7 +30,28 @@ class BotStarter {
                     bot.sendMessage(ChatId.fromId(message.chat.id), "Available commands are: $availableCommands")
                 }
                 command("testPayment") {
-                    bot.sendMessage(ChatId.fromId(message.chat.id), "Hey bruh!")
+                    logger.info("Started testPayment handling")
+                    val response = bot.sendInvoice(
+                        ChatId.fromId(message.chat.id),
+                        PaymentInvoiceInfo(
+                            title = "Тестовый платёж",
+                            description = "Описание",
+                            payload = "internal payload",
+                            providerToken = "my provider payment token",
+                            startParameter = "startParameter",
+                            currency = "USD",
+                            prices = listOf(LabeledPrice(label = "Three hundred bucks", amount = BigInteger.valueOf(30000)))
+                        )
+                    )
+                    logger.info("Got response from telegram: $response")
+                    response.fold(
+                        ifSuccess =  { logger.info("Success!")},
+                        ifError = { bot.sendMessage(ChatId.fromId(message.chat.id), "Something went wrong. $response") }
+                    )
+                }
+                preCheckoutQuery {
+                    logger.info("Started preCheckoutQuery handling")
+                    bot.answerPreCheckoutQuery(preCheckoutQuery.id, ok = true)
                 }
 
                 command("wakeMeUp") {
@@ -42,5 +69,6 @@ class BotStarter {
             "/testPayment",
             "/wakeMeUp",
         )
+        val logger: Logger = Logger.getLogger("BotStarter")
     }
 }
